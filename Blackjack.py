b68823169e7e94c01c3f4e096073440ff21bdbd3
LukasPakalnis <167770753+LukasPakalnis@users.noreply.github.com>
2025-04-29 13:32:07 +0300

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -3,12 +3,14 @@ from art import logo
 
 
 def deal_card():
+    """Returns a random card from the deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 
 def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
@@ -20,6 +22,7 @@ def calculate_score(cards):
 
 
 def compare(u_score, c_score):
+    """Compares the user score u_score against the computer score c_score."""
     if u_score == c_score:
         return "Draw 🙃"
     elif c_score == 0:
